5b267f574f8dc3335cda888fc846dc3a61ab7bf1
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-08-25 08:01:25 +0200

    Bump McReverse version

--- src/main/java/icu/jnet/mccreate/McCreate.java
+++ src/main/java/icu/jnet/mccreate/McCreate.java
@@ -2,25 +2,32 @@ package icu.jnet.mccreate;
 
 import icu.jnet.mcd.api.McClient;
 import icu.jnet.mcd.api.response.Response;
+import icu.jnet.mcd.utils.listener.ClientStateListener;
 
 import java.util.Random;
 
 public class McCreate extends EmailHandler {
 
     private final String domain;
+    private ClientStateListener listener;
 
     public McCreate(String host, int port, String user, String password) {
         super(host, port, user, password);
         this.domain = user.contains("@") ? user.split("@")[1] : "";
     }
 
+    public void setClientStateListener(ClientStateListener listener) {
+        this.listener = listener;
+    }
+
     public RegAccount register(String password, String deviceId) {
         String email = rdmID() + "@" + domain;
-        if(new McClient().register(email, password, "25524", deviceId)) {
+        McClient client = new McClient();
+        client.addStateListener(listener);
+        if(client.register(email, password, "25524", deviceId).success()) {
             String code = searchActivationCode(email, 240);
-            if(code != null && new McClient().activateAccount(email, code, deviceId)) {
-                McClient client = new McClient();
-                if(client.login(email, password, deviceId) && success(client.useMyMcDonalds(true))) {
+            if(code != null && client.activateAccount(email, code, deviceId).success()) {
+                if(client.login(email, password, deviceId).success() && success(client.useMyMcDonalds(true))) {
                     return new RegAccount(email, password, deviceId);
                 }
             }
