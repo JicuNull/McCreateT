1f0ff16e9f26aa6e0ccd565ebd3e39b7a4c1b343
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-10-01 23:50:04 +0200

    Bump McReverse version

--- src/main/java/icu/jnet/mccreate/McCreate.java
+++ src/main/java/icu/jnet/mccreate/McCreate.java
@@ -8,26 +8,28 @@ import java.util.Random;
 
 public class McCreate extends EmailHandler {
 
+    private final Random rand = new Random();
     private final String domain;
-    private ClientStateListener listener;
+    private final ClientStateListener clientListener;
 
-    public McCreate(String host, int port, String user, String password) {
+    public McCreate(String host, int port, String user, String password, ClientStateListener clientListener) {
         super(host, port, user, password);
         this.domain = user.contains("@") ? user.split("@")[1] : "";
+        this.clientListener = clientListener;
     }
 
-    public void setClientStateListener(ClientStateListener listener) {
-        this.listener = listener;
+    public RegAccount register(String password) {
+        return register(password, rdmID(16));
     }
 
     public RegAccount register(String password, String deviceId) {
-        String email = rdmID() + "@" + domain;
+        String email = rdmID(10) + "@" + domain;
         McClient client = new McClient();
-        client.addStateListener(listener);
+        client.addStateListener(clientListener);
         if(client.register(email, password, "25524", deviceId).success()) {
             String code = searchActivationCode(email, 240);
             if(code != null && client.activateAccount(email, code, deviceId).success()) {
-                if(client.login(email, password, deviceId).success() && success(client.useMyMcDonalds(true))) {
+                if(client.login(email, password, deviceId).success() && client.useMyMcDonalds(true).success()) {
                     return new RegAccount(email, password, deviceId);
                 }
             }
@@ -35,12 +37,8 @@ public class McCreate extends EmailHandler {
         return null;
     }
 
-    private String rdmID() {
-        return new Random().ints(97, 123).limit(10)
+    private String rdmID(int length) {
+        return rand.ints(97, 123).limit(length)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
-    }
-
-    private boolean success(Response response) {
-        return response.getStatus().getMessage().contains("successfully");
     }
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,22 @@
 import icu.jnet.mccreate.McCreate;
+import icu.jnet.mcd.api.McClient;
+import icu.jnet.mcd.utils.listener.ClientStateListener;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        McCreate create = new McCreate("host", 993, "user@example.org", "pass");
-        create.register("123", "75408e58622a88c6");
+        ClientStateListener clientListener = createClientListener();
+        McCreate create = new McCreate("host", 993, "user@example.org", "pass", clientListener);
+        create.register("123", McClient.DEFAULT_DEVICE_ID);
+    }
+
+    private static ClientStateListener createClientListener() {
+        return new ClientStateListener() {
+            @Override
+            public String tokenRequired() {
+                return ClientStateListener.super.tokenRequired();
+            }
+        };
     }
 }
