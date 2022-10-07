41de6607ebdd5e2d9ac3d6cd16fde064b7f80c99
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-10-07 22:26:58 +0200

    Update McCreate.java

--- src/main/java/icu/jnet/mccreate/McCreate.java
+++ src/main/java/icu/jnet/mccreate/McCreate.java
@@ -15,6 +15,7 @@ public class McCreate extends McCreateBase {
 
     private final Random rand = new Random();
     private final ClientStateListener clientListener;
+    private final String[] plz = {"22083", "22145", "22399", "23554", "28205", "28197", "30625", "30419"};
 
     public McCreate(ClientStateListener clientListener) {
         this.clientListener = clientListener;
@@ -26,14 +27,18 @@ public class McCreate extends McCreateBase {
 
     public RegAccount register(String password, String deviceId) {
         String address = genRandomMailbox();
+        System.out.println("Email: " + address);
         if(address != null) {
             String username = address.split("@")[0];
             McClient client = new McClient();
             client.addStateListener(clientListener);
-            if(client.register(address, password, String.valueOf(rand.nextInt(80000) + 10000), deviceId).success()) {
+            if(client.register(address, password, plz[rand.nextInt(plz.length)], deviceId).success()) {
+                System.out.println("Register successful");
                 String code = getActivationCode(username);
+                System.out.println("AC: " + code);
                 if(code != null && client.activateAccount(address, code, deviceId).success()) {
                     if(client.login(address, password, deviceId).success() && client.useMyMcDonalds(true).success()) {
+                        System.out.println("Logged in");
                         return new RegAccount(address, password, deviceId);
                     }
                 }
